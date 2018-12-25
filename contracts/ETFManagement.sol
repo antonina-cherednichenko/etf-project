599d0c2684a8578787dc8bda9165578142837879
Antonina Norair <antonina.norair@gmail.com>
2018-12-24 21:00:08 -0800

    Add some logic to smart contracts methods

--- contracts/ETFManagement.sol
+++ contracts/ETFManagement.sol
@@ -4,25 +4,28 @@ contract ETFManagement {
 
   address public crowdsale;
 
-
   constructor(address _crowdsale) public {
     crowdsale = _crowdsale;
   }
 
   function getInfo()
     public
-    constant
+    view
     returns (bytes info)
     {
        return "Security basket for S&P 500: 1) SPDR S&P 500 ETF (SPY), 2) iShares Core S&P 500 ETF (IVV) 3) Vanguard S&P 500 ETF (VOO)"
     }
 
-  function buyETFCoin(address beneficiary, uint256 tokenAmount)
+  function buyETFCoins(uint256 tokenAmount)
     public
     payable
+    returns (bool success)
   {
      require(msg.value >= tokenAmount * getBasketPrice());
-
+     //Buy selected basket of securities, if not succesful refund money
+     //if successfule send tokens to the beneficiary
+     crowdsale.buyTokens.value(msg.value)(msg.sender);
+     return true;
   }
 
 }
