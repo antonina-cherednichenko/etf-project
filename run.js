591f2a249c798e332d65ddd1f3e26daa41c300cf
Antonina Norair <antonina.norair@gmail.com>
2019-01-01 21:33:51 -0800

    Fix errors

--- run.js
+++ run.js
@@ -2,41 +2,40 @@ var request = require('request-promise');
 var opn = require('opn');
 const window = require('window');
 
-function runTradeApi() {
-  var postData = {};
+function getTradeOAuthURL() {
+  return new Promise(function(resolve, reject) {
+    let options = {
+        method: 'post',
+        body: {'apiKey': 'tradeit-test-api-key'},
+        json: true,
+        url: 'https://ems.qa.tradingticket.com/api/v1/preference/getStocksOrEtfsBrokerList'
+    };
 
-  var options = {
-      method: 'post',
-      body: postData,
-      json: true,
-      url: 'https://ems.qa.tradingticket.com/api/v1/preference/getStocksOrEtfsBrokerList'
-  };
-
-  postData.apiKey = 'tradeit-test-api-key';
-
-  request(options)
-    .then(res => {
-       var options = {
-            method: 'post',
-            body: {"apiKey": "tradeit-test-api-key","broker":"Dummy"},
-            json: true,
-            url: 'https://ems.qa.tradingticket.com/api/v1/user/getOAuthLoginPopupUrlForWebApp'
-        };
-      return request(options);
-  })
-    .then(result => {
-      if(result.status == 'ERROR'){
-            console.error('Error getting oAuthURL'); //TODO: error handling
+    request(options)
+      .then(res => {
+        if (res.status == 'ERROR'){
+          reject();
+        } else if (res.status == 'SUCCESS'){
+          var options = {
+              method: 'post',
+              body: {'apiKey': 'tradeit-test-api-key',"broker":"Dummy"},
+              json: true,
+              url: 'https://ems.qa.tradingticket.com/api/v1/user/getOAuthLoginPopupUrlForWebApp'
+          };
+          return request(options);
         }
-        else if(result.status == 'SUCCESS'){
-            var oAuthURL = result.oAuthURL;
-            //window.document.open(oAuthURL, "oAuth Login", "height=435,width=962");
-            opn(oAuthURL).then(res => {
-              console.log("Result of opening = ", res)
-            });
-        }
-      //console.log("RES2 = ", res);
     })
+     .then(res => {
+        if(res.status == 'ERROR'){
+              return reject('Error getting oAuthURL');
+          }
+          else if(res.status == 'SUCCESS'){
+              return resolve(res.oAuthURL);
+          }
+      })
+   });
 }
 
-runTradeApi();
+getTradeOAuthURL().then(res => {
+  console.log("res = ", res)
+})
