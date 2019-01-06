7e2def0f00064c2676686ac70ee7ba0479f846e8
Antonina Norair <antonina.norair@gmail.com>
2019-01-05 22:22:58 -0800

    Add puppeteer

--- run.js
+++ run.js
@@ -1,6 +1,5 @@
 var request = require('request-promise');
-var opn = require('opn');
-const window = require('window');
+const puppeteer = require('puppeteer');
 
 function getTradeOAuthURL() {
   return new Promise(function(resolve, reject) {
@@ -44,9 +43,42 @@ var getTradeItTokens = function(oAuthVerifier) {
       url: 'https://ems.qa.tradingticket.com/api/v1/user/getOAuthAccessToken'
     };
     request(options);
-    
+
 };
 
-getTradeOAuthURL().then(res => {
+getTradeOAuthURL().then(async (res) => {
   console.log("res = ", res)
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(res, {waitUntil: 'networkidle2'});
+  console.log("RES1")
+  //await page.waitFor('input[id="loginUser.credFields"]');
+  console.log("RES2")
+  await page.$('#loginUser', el => el.value = 'dummy');
+  console.log("RES3")
+  await page.$('#loginPwd', el => el.value = 'dummy');
+  console.log("RES4")
+  const loginForm = await page.$('#LoginForm');
+  const res5 = await loginForm.$(loginForm => loginForm.submit());
+
+  await page.evaluate(() => {
+        window.addEventListener('offline', () => {
+            console.log('offline-flag');
+        }, false);
+    });
+
+
+  page.on('onmessage', () => {
+     console.log("HEEERE")
+  })
+  //await page.waitForNavigation();
+  //await page.click('Sign In');
+  //await page.click('button[type="submit"]');
+  console.log("RES5 = ", res5)
+  const text = await page.evaluate(() => {
+        ///const anchor = document.querySelector('#mw-content-text');
+        //return anchor.textContent;
+  });
+  console.log("text = ", text);
+
 })
