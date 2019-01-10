3814c5c0415550b24dd1fb50a986eaa780dd69c6
Antonina Norair <antonina.norair@gmail.com>
2019-01-10 13:59:02 -0800

    Update contracts

--- backend/contracts.js
+++ backend/contracts.js
@@ -1,11 +1,6 @@
 const _ = require('lodash')
 const contract = require('truffle-contract')
 const config = require("../config");
-const Web3 = require("web3");
-
-//define constants
-const DEFAULT_HTTP_PROVIDER = "http://localhost:8545";
-const web3 = new Web3(new Web3.providers.HttpProvider(DEFAULT_HTTP_PROVIDER));
 
 
 let contractsData = {};
@@ -17,11 +12,14 @@ async function getData() {
       return resolve(contractsData);
     }
 
-    let RadiantCoin = contract(require('../build/contracts/RadiantCoin.json'));
-    RadiantCoin.setProvider(web3.currentProvider);
+    let ETFManagement = contract(require('../build/contracts/ETFManagement.json'));
+    ETFManagement.setProvider(config.web3.currentProvider);
 
-    let EventsMonitor = contract(require('../build/contracts/EventsMonitor.json'));
-    EventsMonitor.setProvider(web3.currentProvider);
+    let ETFCoin = contract(require('../build/contracts/ETFCoin.json'));
+    ETFCoin.setProvider(config.web3.currentProvider);
+
+    let ETFCrowdsale = contract(require('../build/contracts/ETFCrowdsale.json'));
+    ETFCrowdsale.setProvider(config.web3.currentProvider);
 
     web3.eth.getAccounts(function(err, accounts) {
       if (err != null) {
@@ -34,15 +32,12 @@ async function getData() {
 
       contractsData.accounts = accounts;
 
-      RadiantCoin.deployed()
-        .then(instance => {
-          contractsData.radiantCoin = instance;
-          return EventsMonitor.deployed();
-         })
-        .then(instance => {
-          contractsData.eventsMonitor = instance;
-          return resolve(contractsData);
-         })
+      ETFCoin.deployed()
+        .then(instance => contractsData.etfCoin = instance;)
+        .then(() => ETFCrowdsale.deployed())
+        .then(instance => contractsData.etfCrowdsale = instance;)
+        .then(() => ETFManagement.deployed())
+        .then(instance => contractsData.etfManagement = instance;)
         .catch(err => reject({error: "Deploying of contracts failed with an error" + err}))
     });
   })
